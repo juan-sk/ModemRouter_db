852741a2479531f57c9f2e2edf72f6405b6c1760
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-05 16:41:21 -0400

    creacion de tabla ticket

---
+++ Tablas/ticket.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS `ticket`;
+create table ticket(
+
+    id_ticket int primary key auto_increment,
+    nombre_cliente varchar(20),
+    rut_cliente varchar(20),
+    telefono varchar(20),
+    detalle varchar(500),
+    observacion varchar(500),
+    estado int,
+    fecha_creacion date default current_timestamp,
+    id_usuario_creacion int,
+    id_usuario_derivado int,
+    id_area int,
+    id_tipo_ticket int
+    
+);

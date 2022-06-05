01d87467fe7a1ca290838d61a2eb8cc6948c1ae0
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-05 16:45:14 -0400

    creacion tabla tipo usuario

---
+++ tipo_usuario.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS `tipo_usuario`;
+create table tipo_usuario(
+
+    id_tipo_usuario int primary key auto_increment,
+    nombre_tipo_usuario varchar(20),
+    dsc_tipo_usuario varchar(50)
+);

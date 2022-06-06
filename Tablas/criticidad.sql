aa43a61c23884e9a8392eabb2e0e93380d4e8d37
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-06 14:54:42 -0400

    se crea tabla criticidad

---
+++ Tablas/criticidad.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS `criticidad`;
+create table criticidad(
+
+    id_criticidad int primary key auto_increment,
+    nom_criticidad varchar(20),
+    dsc_criticidad varchar(50)
+);

eca35659327e37fad7459062019d56e78700d724
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-06 14:55:56 -0400

    se crea tabla tipo ticket

---
+++ Tablas/tipo_ticket.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS `tipo_ticket`;
+create table tipo_ticket(
+
+    id_tipo_ticket int primary key auto_increment,
+    nom_area varchar(20),
+    dsc_tipo_ticket varchar(50)
+);

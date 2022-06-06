8ae70575c36ab246e9d833ada6870ab64fa0970e
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-06 14:57:42 -0400

    se limpia tablas duplicadas

--- tipo_usuario.sql
+++
@@ -1,8 +0,0 @@
-
-DROP TABLE IF EXISTS `tipo_usuario`;
-create table tipo_usuario(
-
-    id_tipo_usuario int primary key auto_increment,
-    nombre_tipo_usuario varchar(20),
-    dsc_tipo_usuario varchar(50)
-);

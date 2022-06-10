34ada79f41c2eaed1b5a53df0b80f49d6fd59f64
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-09 20:42:48 -0400

    cambio largo columnas

--- Tablas/area.sql
+++ Tablas/area.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS `area`;
 create table area(
 
     id_area int primary key auto_increment,
-    nom_area varchar(20),
+    nom_area varchar(50),
     dsc_area varchar(50)
 );
--- Tablas/criticidad.sql
+++ Tablas/criticidad.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS `criticidad`;
 create table criticidad(
 
     id_criticidad int primary key auto_increment,
-    nom_criticidad varchar(20),
+    nom_criticidad varchar(50),
     dsc_criticidad varchar(50)
 );

cdecd0ef792462f9909d89068e23e85e41f99e3b
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-09 20:41:06 -0400

    se agregan tablas de estados

---
+++ Tablas/estado_ticket.sql
@@ -0,0 +1,8 @@
+
+DROP TABLE IF EXISTS `estado_ticket`;
+create table estado_ticket(
+
+    id_estado_ticket int primary key auto_increment,
+    nom_estado_ticket varchar(50),
+    dsc_estado_ticket varchar(50)
+);
